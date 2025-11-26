8e5dc7f1e697bbffdd5dcb05446640eaf1eeac3a
Oleg <luchkoo22@gmail.com>
2025-11-26 10:50:20 +0200

    edit home

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,16 @@
-const Notes = () => {
-    return <div>Notes</div>;
-};
+// 1. Імпортуємо функцію
+import { getNotes } from "@/lib/api";
+import NoteList from "@/components/NoteList/NoteList";
+// 2. Робимо функцію асинхронною та отримуємо дані
+const Notes = async () => {
+  const response = await getNotes();
+// 3. Відображаємо список нотаток
+  return (
+    <section>
+      <h1>Notes List</h1>
+      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+    </section>
+  );
+}
+
 export default Notes;
