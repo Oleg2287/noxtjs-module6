18fb3e2097a6578a1723ec7dc8392ba6908fdd92
Oleg <luchkoo22@gmail.com>
2025-11-27 11:44:45 +0200

    finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
-import TanStackProvider from "@/components/TanStackProvider/TanStackProvider.tsx";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+// app/notes/[id]/NoteDetails.client.tsx
+
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from 'next/navigation';
+import { getSingleNote } from "@/lib/api";
+
 const NoteDetailsClient = () => {
-    return <div>Note Detail Client</div>;
+	const { id } = useParams<{ id: string }>();
+
+  const { data: note, isLoading, error } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => getSingleNote(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error || !note) return <p>Some error..</p>;
+
+  const formattedDate = note.updatedAt
+    ? `Updated at: ${note.updatedAt}`
+    : `Created at: ${note.createdAt}`;
+
+  return (
+    <div>
+      <h2>{note.title}</h2>
+      <p>{note.content}</p>
+      <p>{formattedDate}</p>
+    </div>
+  );
 };
 
 export default NoteDetailsClient;
