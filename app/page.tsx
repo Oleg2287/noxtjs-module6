3b31e1fb166405ac0d6bc79f5fb9929e0096b63f
Oleg <luchkoo22@gmail.com>
2025-12-09 15:01:01 +0200

    add png

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+import Image from 'next/image';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <h1>Welcome to Home</h1>
+      <p>This is the home page.</p>
+      <Image src="/test.png" alt="test" width={300} height={300} />
+    </section>
+  );
 };
+
 export default Home;
