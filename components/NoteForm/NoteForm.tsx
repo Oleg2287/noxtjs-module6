7b28271da0b258002c65fc93d27c1a16c0142fe0
Oleg <luchkoo22@gmail.com>
2025-12-10 15:08:43 +0200

    finish module8

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,37 +1,69 @@
 'use client';
 
+import { Category, createNote, NewNoteData } from '@/lib/api';
+import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
-import { Category } from '@/lib/api';
+// 1. Імпортуємо хук
+import { useNoteDraftStore } from '@/lib/stores/noteStore';
 
 type Props = {
   categories: Category[];
 };
 
 const NoteForm = ({ categories }: Props) => {
-	const router = useRouter();
+  const router = useRouter();
 	
-	const handleCancel = () => router.push('/notes/filter/all');
+	// 2. Викликаємо хук і отримуємо значення
+  const { draft, setDraft, clearDraft } = useNoteDraftStore();
+	
+	// 3. Оголошуємо функцію для onChange щоб при зміні будь-якого 
+	// елемента форми оновити чернетку нотатки в сторі
+  const handleChange = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >,
+  ) => {
+	  // 4. Коли користувач змінює будь-яке поле форми — оновлюємо стан
+    setDraft({
+      ...draft,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: createNote,
+    // 5. При успішному створенні нотатки очищуємо чернетку
+    onSuccess: () => {
+      clearDraft();
+      router.push('/notes/filter/all');
+    },
+  });
 
   const handleSubmit = (formData: FormData) => {
-    const values = Object.fromEntries(formData);
-    console.log(values);
+    const values = Object.fromEntries(formData) as NewNoteData;
+    mutate(values);
   };
-  
+
+  const handleCancel = () => router.push('/notes/filter/all');
+
+	// 6. До кожного елемента додаємо defaultValue та onChange
+	// щоб задати початкове значення із чернетки 
+	// та при зміні оновити чернетку в сторі
   return (
-    <form action={handleSubmit}>
-      <label>
+    <form className={styles.form} action={handleSubmit}>
+      <label className={styles.label}>
         Title
-        <input type="text" name="title" />
+        <input type="text" name="title" defaultValue={draft?.title} onChange={handleChange} />
       </label>
 
-      <label>
+      <label className={styles.label}>
         Content
-        <textarea name="content"></textarea>
+        <textarea name="content" defaultValue={draft?.content} onChange={handleChange}></textarea>
       </label>
 
-      <label>
+      <label className={styles.label}>
         Category
-        <select name="category">
+        <select name="category" defaultValue={draft?.categoryId} onChange={handleChange} >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
               {category.name}
@@ -40,7 +72,7 @@ const NoteForm = ({ categories }: Props) => {
         </select>
       </label>
 
-      <div>
+      <div className={styles.actions}>
         <button type="submit">Create</button>
         <button type="button" onClick={handleCancel}>Cancel</button>
       </div>
@@ -49,3 +81,125 @@ const NoteForm = ({ categories }: Props) => {
 };
 
 export default NoteForm;
+
+
+
+
+// 'use client';
+
+// import { useMutation } from '@tanstack/react-query';
+// import { Category, createNote, NewNoteData } from '@/lib/api';
+
+// type Props = {
+//   categories: Category[];
+// };
+
+// const NoteForm = ({ categories }: Props) => {
+//   const { mutate } = useMutation({
+//     mutationFn: createNote,
+//     onSuccess: () => {
+//       router.push('/notes/filter/all');
+//     },
+//   });
+
+//   const handleCancel = () => router.push('/notes/filter/all');
+
+//   const handleSubmit = (formData: FormData) => {
+//     const values = Object.fromEntries(formData) as NewNoteData;
+//     mutate(values);
+//   };
+  
+//   // Решта коду компонента
+
+//   return (
+//     <form action={handleSubmit}>
+// 			<label>
+//         Title
+//         <input type="text" name="title" />
+//       </label>
+
+//       <label>
+//         Content
+//         <textarea name="content"></textarea>
+//       </label>
+
+//       <label>
+//         Category
+//         <select name="category">
+//           {categories.map((category) => (
+//             <option key={category.id} value={category.id}>
+//               {category.name}
+//             </option>
+//           ))}
+//         </select>
+//       </label>
+
+//       <div>
+//         <button type="submit">Create</button>
+//         <button type="button" onClick={handleCancel}>Cancel</button>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default NoteForm;
+
+
+
+
+
+
+
+
+
+// 'use client';
+
+// import { useRouter } from 'next/navigation';
+// import { Category } from '@/lib/api';
+
+// type Props = {
+//   categories: Category[];
+// };
+
+// const NoteForm = ({ categories }: Props) => {
+// 	const router = useRouter();
+	
+// 	const handleCancel = () => router.push('/notes/filter/all');
+
+//   const handleSubmit = (formData: FormData) => {
+//     const values = Object.fromEntries(formData);
+//     console.log(values);
+//   };
+  
+//   return (
+//     <form action={handleSubmit}>
+//       <label>
+//         Title
+//         <input type="text" name="title" />
+//       </label>
+
+//       <label>
+//         Content
+//         <textarea name="content"></textarea>
+//       </label>
+
+//       <label>
+//         Category
+//         <select name="category">
+//           {categories.map((category) => (
+//             <option key={category.id} value={category.id}>
+//               {category.name}
+//             </option>
+//           ))}
+//         </select>
+//       </label>
+
+//       <div>
+//         <button type="submit">Create</button>
+//         <button type="button" onClick={handleCancel}>Cancel</button>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default NoteForm;
